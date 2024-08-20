22c0db3d702be03cf05e7bbe89a214c9b6aa721e
Grant Birchmeier <gbirchmeier@connamara.com>
2024-08-20 15:30:39 -0500

    new compose, new sln, DemoSender

---
+++ DemoSender/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace DemoSender
+{
+    public static class Program
+    {
+        private static void Main(string[] args) {
+            if (args.Length < 1)
+                throw new InvalidOperationException("Need a param, which is the msg to send");
+
+            const string topic = "demo-topic";
+
+            var config = new ProducerConfig
+            {
+                // User-specific properties that you must set
+                BootstrapServers = "localhost:9092",
+
+                // Fixed properties
+                Acks = Acks.All
+            };
+
+            var msg = new Message<string, string> { Key = "defaultKey", Value = args.First() };
+
+            using (var producer = new ProducerBuilder<string, string>(config).Build()) {
+                producer.Produce(topic, msg,
+                    (deliveryReport) =>
+                    {
+                        if (deliveryReport.Error.Code != ErrorCode.NoError) {
+                            Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                        } else {
+                            Console.WriteLine($"Produced event to topic {topic}: '{msg.Value}'");
+                        }
+                    });
+                producer.Flush(TimeSpan.FromSeconds(10));
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-spike of a system with a dotnet bus and protobufs
+# spike of a system with a dotnet bus and protobufs
+
+
+
+https://kafka.apache.org/quickstart
+
+$ docker pull apache/kafka:3.8.0
+$ docker run -p 9092:9092 apache/kafka:3.8.0
+
+
+./bin/kafka-topics.sh --create --topic quickstart-events --bootstrap-server localhost:9092
+./bin/kafka-console-producer.sh --topic quickstart-events --bootstrap-server localhost:9092
+./bin/kafka-console-consumer.sh --topic quickstart-events --from-beginning --bootstrap-server localhost:9092
+./bin/kafka-console-consumer.sh --topic quickstart-events --from-beginning --bootstrap-server localhost:9092
+
+
+docker compose -f docker-compose.yml up --build
+
+
+
+shell into an image:
+docker exec -it <image> bash
+
+on image:
+./opt/kafka/bin/
